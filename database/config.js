7c9cd0d75caf4679955d138aab13d10395f8b98e
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-10-19 20:27:17 -0500

    a

--- database/config.js
+++ database/config.js
@@ -2,9 +2,11 @@
 import mongoose from "mongoose"
 import "dotenv/config" 
 
+const Co = "XMONGODB_URL = mongodb+srv://bldm147:5eI0JHrqszPDO7rb@cluster0.npwmfxa.mongodb.net/dbapi?retryWrites=true&w=majority"
+
 export async function connect() {
     try {
-        await mongoose.connect(process.env.XMONGODB_URL)
+        await mongoose.connect(Co)
         console.log("Conectado con Mogodb")
     } catch (error) {
         
