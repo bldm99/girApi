e0c0e3958e720f2d8660b21dc271c0e2084d162a
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-12-26 18:08:08 -0500

    login6

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 //app.use(cors())
 app.use(
     cors({
-      origin: "http://localhost:5173/login", // Reemplaza esto con la URL de tu aplicación React
+      origin: "http://localhost:5173", // Reemplaza esto con la URL de tu aplicación React
       credentials: true,
     })
 );
